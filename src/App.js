89dab97a56a8903b4ac52f3c8aaf587ab5097f84
Gela <68652298+LuQ1z@users.noreply.github.com>
2023-01-22 23:33:26 +0400

    daily update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
 
-const categories = [
-	{
-		id: 1,
-		title: "Hats",
-		imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-	},
-	{
-		id: 2,
-		title: "Jackets",
-		imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-	},
-	{
-		id: 3,
-		title: "Sneakers",
-		imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-	},
-	{
-		id: 4,
-		title: "Womans",
-		imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-	},
-	{
-		id: 5,
-		title: "Mens",
-		imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-	},
-];
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+	return <h1>Shop Template</h1>;
+};
 
 const App = () => {
-	return <Directory categories={categories} />;
+	return (
+		<Routes>
+			<Route path="/" element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path="shop" element={<Shop />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
